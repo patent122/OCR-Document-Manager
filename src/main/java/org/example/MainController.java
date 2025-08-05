804b371cd21eca9eeecc353f2f26de2bc8d70c37
Patryk <patrykpanek3@gmail.com>
2025-08-05 19:09:21 +0200

    OCR Document Manager

---
+++ src/main/java/org/example/MainController.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+
+    public void onLoadDocumentClick(ActionEvent event) {
+        System.out.println("Kliknięto Wczytaj dokument (FXML)");
+    }
+}
